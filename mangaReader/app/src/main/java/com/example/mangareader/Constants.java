e52fcd6afd91c15fccc29e270eb0f8f62d621144
Pawan Maurya <pawanabc59@gmail.com>
2020-11-05 22:20:43 +0530

    New apk released

--- mangaReader/app/src/main/java/com/example/mangareader/Constants.java
+++ mangaReader/app/src/main/java/com/example/mangareader/Constants.java
@@ -2,8 +2,9 @@ package com.example.mangareader;
 
 public class Constants {
 
-//    public static final String main_path = "http://192.168.43.151:5656";
-    public static final String main_path = "https://mreader.herokuapp.com";
+//    public static final String main_path = "http://192.168.0.105:5656";
+//    public static final String main_path = "https://mreader.herokuapp.com";
+    public static final String main_path = "https://m-reader.herokuapp.com/";
     public static final String url_login = main_path+"/user/login";
     public static final String url_get_chapters = main_path+"/manga/getChapters";
     public static final String url_add_favourites = main_path+"/user/favourite/add";
